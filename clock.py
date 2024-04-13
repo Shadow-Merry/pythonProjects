874f2b259fb1007dbc2a0204799dc47620916692
merry abraham <merryrock6@gmail.com>
2024-04-13 13:00:16 +0300

    Final Touch

--- clock.py
+++ clock.py
@@ -7,26 +7,36 @@ def clear():
         os.system('cls')
     else:
         os.system('clear')
+
+def numberMonth(new):
+        if new.month == 4 or new == 6 or new == 9 or new == 11:
+            monthDay = 30
+        elif new.month == 2:
+            if new.year % 100 == 0:
+                if new.year % 400 == 0:
+                    monthDay = 29
+                else:
+                    monthDay = 28
+            else:
+                if new.year % 4 == 0:
+                    monthDay = 29
+                else:
+                    monthDay = 28
+        else:
+            monthDay = 31
+        return monthDay
+
+def screen():
+    print("Wellcome to Clock program")
+    print("This program Lets you know How Many Month and Day are Left ")
+    print()
 try:
     clear()
     new = datetime.date.today()
-    date = datetime.datetime.strptime(input("Enter the Date: "), '%Y-%m-%d')
+    screen()
+    date = datetime.datetime.strptime(input("Enter the Last Date: "), '%Y-%m-%d')
     inputDate = date.date()
-    if new.month == 4 or new == 6 or new == 9 or new == 11:
-        monthDay = 30
-    elif new.month == 2:
-        if new.year % 100 == 0:
-            if new.year % 400 == 0:
-                monthDay = 29
-            else:
-                monthDay = 28
-        else:
-            if new.year % 4 == 0:
-                monthDay = 29
-            else:
-                monthDay = 28
-    else:
-        monthDay = 31
+    monthDay = numberMonth(new)
     if inputDate.year < new.year:
         print(f"Enter the Year Correctly You Enter {inputDate.year}")
         year = 0
@@ -48,10 +58,18 @@ try:
         date = 0
     else:
         date = inputDate.day - new.day
-    print(f"End Date: {inputDate}")
+    print()
+    print("*************************")
     print(f"Started Date: {new}")
+    print(f"End Date: {inputDate}")
+    print("*************************")
+    
     print(f"You Have {month} Month And {date} Day")
-    time.sleep(5)
+    print()
+    time.sleep(10)
 
 except ValueError:
-    print("Please Enter The Date in This Formate YEAR-MONTH-DAY")
+    print("Please Enter The Date Correctly")
+    print("And The Formate is YEAR-MONTH-DAY")
+    print()
+    time.sleep(10)
